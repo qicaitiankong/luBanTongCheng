496c58fa5bdfd717bb618a74a11208a9b4e15879
apple <apple@appledemacbook-pro.local>
2018-07-24 17:06:08 +0800

    地图制作完成

--- 鲁班同城/接单大厅/雇主版/C/DispatchOrderMapViewController.m
+++ 鲁班同城/接单大厅/雇主版/C/DispatchOrderMapViewController.m
@@ -11,16 +11,21 @@
 #import <MAMapKit/MAMapKit.h>
 #import <AMapSearchKit/AMapSearchKit.h>
 #import "GeocodeAnnotation.h"
-#import "CommonUtility.h"
+#import "POIAnnotation.h"
+
 //
 #import "LaunchPiesViewController.h"
 #import "searchHeaderView.h"
 
 @interface DispatchOrderMapViewController ()<MAMapViewDelegate,AMapSearchDelegate>{
+    //当前用户的真实定位位置
     CLLocation *location;
+    //搜索框
     searchHeaderView *searchView;
-    AMapGeocodeSearchRequest *searchGeo;
+    //搜素搜返回信息
     AMapSearchAPI *searchAPI;
+    //进来地图缩放等级
+    double zoomLevel;
 }
 
 @property (nonatomic, strong) MAMapView *mapView;
@@ -47,7 +52,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"派单";
     self.view.backgroundColor = [UIColor whiteColor];
     [NavTools hiddenNav:self.navigationController];
     [NavTools hiddenTabbar:self.rdv_tabBarController];
@@ -58,58 +62,111 @@
     //
     [self addMapView];
     //
+    [self addDisttributeOrderButt];
+    //
     [self addLocationButt];
 }
 
 - (void)initOwnObjects{
-     self.dispatchOrderImage = [UIImage imageNamed:@"dispatchOnMap"];
+    zoomLevel = 14;
+    self.annotations = [[NSMutableArray alloc]init];
+    self.dispatchOrderImage = [UIImage imageNamed:@"dispatchOnMap"];
     searchAPI = [[AMapSearchAPI alloc]init];
-    searchGeo = [[AMapGeocodeSearchRequest alloc]init];
     searchAPI.delegate = self;
 }
 
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
-    MAMapView *mapView = self.mapView;
-    location =  mapView.userLocation.location;
-    NSLog(@"定位位置：location.altitude%lf,location.coordinate%lf",location.coordinate.latitude,location.coordinate.longitude);
+    location =  self.mapView.userLocation.location;
+    //NSLog(@"定位位置：location.altitude%lf,location.coordinate%lf",location.coordinate.latitude,location.coordinate.longitude);
     if (self.annotations.count == 0){
         [self addAnnotations:location.coordinate];
+        //[self.mapView showAnnotations:self.annotations animated:YES];
+        [self.mapView setZoomLevel:zoomLevel];
     }
 }
 //logic
 - (void)searchTextField:(NSString*)targetStr{
-    searchGeo.address = targetStr;
-    [searchAPI AMapGeocodeSearch:searchGeo];
+     AMapPOIKeywordsSearchRequest *searchPoi = [[AMapPOIKeywordsSearchRequest alloc]init];
+    searchPoi.keywords = targetStr;
+    //    request.keywords            = @"北京大学";
+    //    request.city                = @"北京";
+    //    request.types               = @"高等院校";
+    //    request.requireExtension    = YES;
+    //
+    //    /*  搜索SDK 3.2.0 中新增加的功能，只搜索本城市的POI。*/
+    //    request.cityLimit           = YES;
+    searchPoi.requireSubPOIs      = YES;
+    
+    [searchAPI AMapPOIKeywordsSearch:searchPoi];
 }
-
-- (void)onGeocodeSearchDone:(AMapGeocodeSearchRequest *)request response:(AMapGeocodeSearchResponse *)response
+//poi 搜索回调
+- (void)onPOISearchDone:(AMapPOISearchBaseRequest *)request response:(AMapPOISearchResponse *)response
 {
-    if (response.geocodes.count == 0)
+
+    if (response.pois.count == 0)
     {
         return;
     }
-    //解析response获取地理信息
-    NSMutableArray *annotations = [NSMutableArray array];
-    
-    [response.geocodes enumerateObjectsUsingBlock:^(AMapGeocode *obj, NSUInteger idx, BOOL *stop) {
-        GeocodeAnnotation *geocodeAnnotation = [[GeocodeAnnotation alloc] initWithGeocode:obj];
-        [annotations addObject:geocodeAnnotation];
+
+    NSMutableArray *poiAnnotations = [NSMutableArray arrayWithCapacity:response.pois.count];
+
+    [response.pois enumerateObjectsUsingBlock:^(AMapPOI *obj, NSUInteger idx, BOOL *stop) {
+        [poiAnnotations addObject:[[POIAnnotation alloc] initWithPOI:obj]];
     }];
-    
-    if (annotations.count == 1)
+    /* 如果只有一个结果，设置其为中心点. */
+    if (poiAnnotations.count == 1)
     {
-        [self.mapView setCenterCoordinate:[annotations[0] coordinate] animated:YES];
+        [self.mapView setCenterCoordinate:[poiAnnotations[0] coordinate]];
     }
+    /* 如果有多个结果, 设置地图使所有的annotation都可见. */
     else
     {
-        [self.mapView setVisibleMapRect:[CommonUtility minMapRectForAnnotations:annotations]
-                               animated:YES];
+        [self.mapView showAnnotations:poiAnnotations animated:NO];
     }
-    [self addAnnotations:[annotations[0] coordinate]];
-    
+    if (poiAnnotations.count){
+        [self addAnnotations:[poiAnnotations[0] coordinate]];
+        [self.mapView setZoomLevel:zoomLevel];
+    }else{
+        NSLog(@"没有找到");
+    }
 }
-//logic end
+
+- (void)AMapSearchRequest:(id)request didFailWithError:(NSError *)error
+{
+    //搜索出错
+    NSLog(@"Error: %@ - error.code= %ld", error, error.code);
+}
+
+//用户位置图标设置
+- (void)mapView:(MAMapView *)mapView didAddAnnotationViews:(NSArray *)views
+{
+    MAAnnotationView *view = views[0];
+    
+    // 放到该方法中用以保证userlocation的annotationView已经添加到地图上了。
+    if ([view.annotation isKindOfClass:[MAUserLocation class]])
+    {
+        MAUserLocationRepresentation *pre = [[MAUserLocationRepresentation alloc] init];
+        pre.image = [UIImage imageNamed:@"userPosition"];
+        pre.fillColor = RGBA(44, 101, 236, 0.6);
+        pre.strokeColor = [UIColor whiteColor];
+        pre.lineWidth = 3;
+        [self.mapView updateUserLocationRepresentation:pre];
+        view.calloutOffset = CGPointMake(0, 0);
+        view.canShowCallout = NO;
+        self.userLocationAnnotationView = view;
+    }
+}
+- (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation
+{
+    if (!updatingLocation && self.userLocationAnnotationView != nil)
+    {
+        [UIView animateWithDuration:0.1 animations:^{
+            double degree = userLocation.heading.trueHeading;
+            self.userLocationAnnotationView.transform = CGAffineTransformMakeRotation(degree * M_PI / 180.f );
+        }];
+    }
+}
 
 //
 - (void)addMapView{
@@ -122,99 +179,82 @@
     self.mapView.showsScale = NO;
     self.mapView.showsCompass = NO;
     //添加大头针
-    self.mapView.showsUserLocation = YES;
+    //self.mapView.showsUserLocation = YES;
     self.mapView.userTrackingMode = 2;
     [self addLocationButt];
     //
 }
 
-- (void)mapView:(MAMapView *)mapView didAddAnnotationViews:(NSArray *)views
-{
-
-    for (int k = 0; k < views.count; k ++){
-        MAAnnotationView *view = views[k];
-        // 放到该方法中用以保证userlocation的annotationView已经添加到地图上了。
-        if ([view.annotation isKindOfClass:[MAUserLocation class]])
-        {
-            MAUserLocationRepresentation *pre = [[MAUserLocationRepresentation alloc] init];
-            //pre.fillColor = SPECIAL_BLUE_COLOR;
-            pre.strokeColor = SPECIAL_BLUE_COLOR;
-            if (k == 0){
-                 [pre setImage:self.dispatchOrderImage];
-                //self.userLocationAnnotationView = view;
-
-            }else{
-
-            }
-            pre.lineWidth = 3;
-           
-            //[self.mapView updateUserLocationRepresentation:pre];
-            }
-    }
+- (void)addDisttributeOrderButt{
+    WS(weakSelf);
+    CustomerImageButt *disttributeButt = [[CustomerImageButt alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH * 0.373, SCREEN_WIDTH * 0.373 * 0.39)];
+    disttributeButt.center = CGPointMake(self.view.width / 2, SCREEN_HEIGHT / 2 - searchView.height - STATUSBAR_HEIGHT);
+    //disttributeButt.backgroundColor = [UIColor whiteColor];
+    [disttributeButt.imageView setImage:[UIImage imageNamed:@"dispatchOnMap"]];
+    disttributeButt.clickButtBlock = ^{
+        LaunchPiesViewController *launchVC = [[LaunchPiesViewController alloc]init];
+        [weakSelf.navigationController pushViewController:launchVC animated:YES];
+    };
+    [self.view addSubview:disttributeButt];
 }
+
 //动态添加标记
 - (void)addAnnotations:(CLLocationCoordinate2D)targetLocation{
-    if (self.annotations){
+    if (self.annotations.count){
         [self.mapView removeAnnotations:self.annotations];
         [self.annotations removeAllObjects];
     }
-    self.annotations = [NSMutableArray array];
     double latitudeValue = targetLocation.latitude;
     double longitudeValue = targetLocation.longitude;
     //模拟随机位置标注经纬度
-    NSInteger moniCount = 32;
+    NSInteger moniCount = 45;
     NSInteger perGroupCount = moniCount / 4;
     for (int i = 0; i < moniCount; i ++){
         double newLatitude = 0;
         double newLongitude = 0;
         
-        int x = arc4random() % 10 + 4;
+        int x = arc4random() % 80 + 4;
         double randomDoubleX = x / 3000.000;
         //
-        int y = arc4random() % 10 + 1;
+        int y = arc4random() % 80 + 4;
         double randomDoubleY = y / 3000.000;
         //
-        if (i < perGroupCount){
-            if (i == 0){
-                newLatitude = latitudeValue;
-                newLongitude = longitudeValue;
-            }else{
+        if (i < perGroupCount){//右上方
+            
                 newLatitude = latitudeValue + randomDoubleX;
                 newLongitude = longitudeValue + randomDoubleY;
-            }
-            
+        
         }else if ((i > perGroupCount ) && (i < perGroupCount * 2)){
-            if (i / 2){
+            if (i / 2){//左下
                 newLatitude = latitudeValue - randomDoubleX;
                 newLongitude = longitudeValue - randomDoubleY;
-            }else{
+            }else{//左
                 newLatitude = latitudeValue;
                 newLongitude = longitudeValue - randomDoubleY;
             }
-        }else if (i > perGroupCount * 2 && i < perGroupCount * 3){
-            if (i / 2){
+        }else if ((i > perGroupCount * 2) && (i < perGroupCount * 3)){
+            if (i / 2){//左上
                 newLatitude = latitudeValue + randomDoubleX;
                 newLongitude = longitudeValue - randomDoubleY;
-            }else{
+            }else{//右
                 newLatitude = latitudeValue;
                 newLongitude = longitudeValue + randomDoubleY;
             }
             
-        }else{
+        }else{//右下
             newLatitude = latitudeValue - randomDoubleX;
             newLongitude = longitudeValue + randomDoubleY;
         }
-        //
-        MAPointAnnotation *a1 = [[MAPointAnnotation alloc] init];
         CLLocationCoordinate2D coordinate = {newLatitude,newLongitude};
+        MAPointAnnotation *pointAnotion = [[MAPointAnnotation alloc]init];
+        pointAnotion.coordinate = coordinate;
         //NSLog(@"newLatitude=%lf,newLongitude=%lf",newLatitude,newLongitude);
-        a1.coordinate = coordinate;
-        [self.annotations addObject:a1];
+        [self.annotations addObject:pointAnotion];
     }
     //
     [self.mapView addAnnotations:self.annotations];
-    //[self.mapView setVisibleMapRect:[CommonUtility minMapRectForAnnotations:self.annotations]
-                      //     animated:YES];
+    [self.mapView setCenterCoordinate:targetLocation];
+    
 }
 
 /*!
@@ -226,23 +266,24 @@
 - (MAAnnotationView*)mapView:(MAMapView *)mapView viewForAnnotation:(id <MAAnnotation>)annotation {
     if ([annotation isKindOfClass:[MAPointAnnotation class]])
     {
-        
+        //NSLog(@"viewForAnnotation 执行了");
         static NSString *pointReuseIndetifier = @"pointReuseIndetifier";
         MAPinAnnotationView *annotationView = (MAPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:pointReuseIndetifier];
         if (annotationView == nil)
         {
             annotationView = [[MAPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:pointReuseIndetifier];
         }
+        
         annotationView.canShowCallout               = NO;
+        //实现自定义图片必须关闭动画效果
         annotationView.animatesDrop                 = NO;
-        annotationView.draggable                    = YES;
-        annotationView.image = [UIImage imageNamed:@"work_person"];
+        annotationView.draggable                    = NO;
+        [annotationView setImage:[UIImage imageNamed:@"work_person2"]];
         return annotationView;
     }
-    
+    NSLog(@"viewForAnnotation 没有执行");
     return nil;
 }
-
 
 
 /*!
@@ -252,11 +293,6 @@
  */
 - (void)mapView:(MAMapView *)mapView didSelectAnnotationView:(MAAnnotationView *)view {
     NSLog(@"annotationView 点击");
-    if([view.image isEqual:self.dispatchOrderImage]){
-        LaunchPiesViewController *launchVC = [[LaunchPiesViewController alloc]init];
-        [self.navigationController pushViewController:launchVC animated:YES];
-    }
-    
 }
 
 /**
@@ -267,6 +303,18 @@
  */
 - (void)mapView:(MAMapView *)mapView didAnnotationViewCalloutTapped:(MAAnnotationView *)view {
     
+}
+
+-(void)mapView:(MAMapView *)mapView mapDidMoveByUser:(BOOL)wasUserAction{
+    if (wasUserAction){
+        //平移式自动生成标注显示
+        [self addAnnotations:self.mapView.centerCoordinate];
+    }
+}
+
+-(void)mapView:(MAMapView *)mapView mapDidZoomByUser:(BOOL)wasUserAction{
+}
+-(void)mapView:(MAMapView *)mapView mapWillZoomByUser:(BOOL)wasUserAction{
 }
 
 - (void)addSearchView{
@@ -344,8 +392,17 @@
     if(self.mapView.userLocation.updating && self.mapView.userLocation.location) {
         [self.mapView setCenterCoordinate:self.mapView.userLocation.location.coordinate animated:YES];
         [self.gpsButton setSelected:YES];
+        //重新生成标记
+        [self addAnnotations:location.coordinate];
+        [self.mapView setZoomLevel:zoomLevel];
     }
 }
+//地图视角居中显示
+//-(void)centerMapViewSetting{
+//    if(self.mapView.userLocation.updating && self.mapView.userLocation.location) {
+//        [self.mapView setCenterCoordinate:self.mapView.userLocation.location.coordinate animated:YES];
+//    }
+//}
 
 /*!
  @brief 拖动annotation view时view的状态变化，ios3.2以后支持
@@ -357,7 +414,7 @@
 - (void)mapView:(MAMapView *)mapView annotationView:(MAAnnotationView *)view didChangeDragState:(MAAnnotationViewDragState)newState fromOldState:(MAAnnotationViewDragState)oldState {
     
 }
-
+//
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
